137d810482cd77d53a94d7b85799cc9191d04539
bg <barakgonen9@gmail.com>
2025-07-06 08:53:40 +0300

    Init commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ src/main/java/com/example/data/publisher/InterfaceDataPublisher.java
@@ -0,0 +1,5 @@
+package com.example.data.publisher;
+
+public interface InterfaceDataPublisher {
+    void execute();
+}
